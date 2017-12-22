7c25306bc85a1fe8bf2a8ddb45d923c1ac799b3e
intothedeep <victor.taek.lim@gmail.com>
2017-12-22 17:55:22 +0900

    complete .eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,8 @@ module.exports = {
     ],
     "env": {
       "browser": true
-    }
+    },
+    rules:{
+      "linebreak-style": 0
+    }    
 };
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,6 +1,8 @@
 // entry.js
+import random from 'lodash/random';
 import '../css/common.css';
 import hello from './hello';
 import world from './world';
-var a = 0; // with eslint, error ocurrs!
+
 document.write(`${hello}, ${world}!`);
+document.write(`Random: ${random(0, 100)}`);
